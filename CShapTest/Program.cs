3dd185f55628ad16903f54872c8c54eb438999bf
HYUK LEE <ruldes76@gmail.com>
2020-03-17 22:57:44 +0900

    commit

--- CShapTest/Program.cs
+++ CShapTest/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace CShapTest
 {
@@ -6,68 +7,92 @@ namespace CShapTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Fibonacci World!");
+            Console.WriteLine("{0}, Hello Fibonacci World!", DateTime.Now.ToString("MM/dd/yyyy hh:mm:ss.fff tt"));
 
-            Fibonacci(10);
+
+            Task t1 = new Task(() =>
+            {
+                Console.WriteLine(String.Format("t1 >>> {0}", DateTime.Now.ToString("MM/dd/yyyy hh:mm:ss.fff tt")));
+
+                Fibonacci(20);
+
+                Console.WriteLine(String.Format("t1 >>> {0}", DateTime.Now.ToString("MM/dd/yyyy hh:mm:ss.fff tt")));
+            });
+
+
+
+            Task t2 = new Task(() =>
+            {
+                Console.WriteLine(String.Format("t2 >>> {0}", DateTime.Now.ToString("MM/dd/yyyy hh:mm:ss.fff tt")));
+
+                int toValue = 20;
+                for (int i = 1; i <= toValue; i++)
+                {
+                    fibo(i);
+                    //Console.WriteLine("{0} ", fibo(i));
+                }
+
+                Console.WriteLine(String.Format("t2 >>> {0}", DateTime.Now.ToString("MM/dd/yyyy hh:mm:ss.fff tt")));
+            });
+
+
+
+            // Task 쓰레드 시작
+            t1.Start();
+            t2.Start();
+
+            // Task가 끝날 때까지 대기
+            t1.Wait();
+            t2.Wait();
+
+
+
+
 
 
         }
+        static long fibo(int value)
+        {
+            if (value == 1 || value == 2)
+            {
+                return 1;
+            }
+            /*
+            3(2 +1)
 
-        private static void Fibonacci(int limit)
+
+            )
+             */
+
+            //Console.WriteLine("{0}, {1}", fibo(value - 1), fibo(value - 2));
+
+            return fibo(value - 1) + fibo(value - 2);
+        }
+        static void Fibonacci(int limit)
         {
             var x = 0;
             var y = 1;
+            int result = 0;
 
             for (int i = 0; i < limit; i++)
             {
 
-                int result = x + y;
+                result = x + y;
 
 
-                Console.WriteLine(String.Format("x:{0}, y:{1}, result:{2}", x, y, result));
+                //Console.WriteLine(String.Format("x:{0}, y:{1}, result:{2}", x, y, result));
 
 
                 x = y;
                 y = result;
 
 
-
-
             }
-
-            /* 0,1,1,2,3,5,8,13 */
-
-            /* 0,1,1
-             x = 0
-             y = x + 1
-             result = x + y (1)
-             */
-
-            /* 1,1,2
-            x = y (1)
-            y = result (1)
-            result = x + y (2)
-
-             */
-            /* 1,2,3
-            x = y (1)
-            y = result (2)
-            result = x + y (3)
-             */
-
-            /* 2,3,5
-            x = y (2)
-            y = result (3)
-            result = x + y (5)
-             */
-
-
-            /* 3,5,8
-            x = y (3)
-            y = result (5)
-            result = x + y (8)
-             */
+            Console.WriteLine(String.Format("{0}", DateTime.Now.ToString("MM/dd/yyyy hh:mm:ss.fff tt")));
+            
 
         }
+    
     }
 }
+
