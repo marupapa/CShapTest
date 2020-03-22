22aece650b46cc2f261b06a8cf63142c66290735
HYUK LEE <ruldes76@gmail.com>
2020-03-22 22:23:19 +0900

    commit

--- CShapTest/Program.cs
+++ CShapTest/Program.cs
@@ -8,6 +8,7 @@ namespace CShapTest
         static void Main(string[] args)
         {
             /* 与えられた整数の配列の中から足りない数字を探す */
+            /*
             int[] arrayInt = new int[] { 1, 2, 3, 4, 5, 6, 8, 9 };
 
             int result = 0;
@@ -24,6 +25,60 @@ namespace CShapTest
             }
 
             Console.WriteLine("{0}", result);
+            /*
+
+            /* ソートされていない整数の配列から最大値と最小値を探す */
+            //int[] arrayInt = new int[] { 3, 6, 5, 4, 8, 9, 10, 13 };
+
+            //int nMin = 0, nMax = 0;
+
+            //for (int x = 0; x < arrayInt.Length; x++)
+            //{
+            //    nMin = arrayInt[x];
+            //    nMax = arrayInt[x];
+
+            //    for (int i = 0; i < arrayInt.Length; i++)
+            //    {
+            //        /*最小数*/
+            //        if (nMin > arrayInt[i]) nMin = arrayInt[i];
+            //        /*最大数*/
+            //        if (nMax < arrayInt[i]) nMax = arrayInt[i];
+            //    }
+            //}
+
+            //Console.WriteLine("{0} : {1}", nMin, nMax);
+
+            /* 合計すると与えられた数字と同じになる整数の配列のすべての組み合わせ */
+            int[] array = new int[] { 3, 6, 5, 4, 8, 9, 10, 13 };
+
+            int nSumValue = 23;
+            int nValue = 0;
+            int nSumResult = 0;
+            for (int x = 0; x < array.Length - 1; x++)
+            {
+                nValue = array[x];
+                for (int i = 1; i < array.Length; i++)
+                {
+
+                    nSumResult = nValue + array[i];
+
+                    if (nSumValue.Equals(nSumResult))
+                    {
+                        Console.WriteLine("{0} + {1} = {2} ", nValue, array[i], nSumResult);
+
+
+                        break;
+                    }
+
+
+                }
+
+            }
+
+            //Console.WriteLine("{0} ", nSumResult);
+
+
+
 
 
             //Console.WriteLine("{0}, Hello Fibonacci World!", DateTime.Now.ToString("MM/dd/yyyy hh:mm:ss.fff tt"));
